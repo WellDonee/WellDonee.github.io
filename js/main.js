d8c074b3033c0907ef623af1a964566dd35e6efe
WellDonee <30930868+WellDonee@users.noreply.github.com>
2018-01-15 22:03:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var button = document.querySelector('.backCalltoggleButton');
+
+button.onclick = function() {
+	var element = document.querySelector('.backCallForm');
+	var arrow = document.querySelector('.backCalltoggleButton::after');
+	if (element.classList.contains('hide')) {
+		element.classList.remove('hide');
+		arrow.style.transform = 'rotate(0deg)';
+	} else {
+		element.classList.add('hide');
+		arrow.style.transform = 'rotate(90deg)';
+	}
+}
